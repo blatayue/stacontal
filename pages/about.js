75a145ac2605282fe0c8a48fb3acfc76da4363e5
Andrew-Morris <blatadude@gmail.com>
2018-01-17 15:40:43 -0700

    tutorial level

---
+++ pages/about.js
@@ -0,0 +1,6 @@
+const about = () => (
+    <div>
+        <p>Something about this page is odd.</p>
+    </div>
+)
+export default about
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+const index = () => (
+    <div>
+        <Link href="/about">
+            <a>About Page</a>
+        </Link>
+        <p>Hello Next.js</p>
+    </div>
+)    
+export default index
